d24fbd297b3a469e93239769a15ca75c6eca7d0e
videokent <anton.bem@gmail.com>
2020-12-14 21:24:07 +0300

    cors

--- server/index.js
+++ server/index.js
@@ -1,8 +1,9 @@
  var express = require("express");
+ cors = require("cors");
  var bodyParser = require('body-parser');
 var app = express();
 var urlencodedParser = bodyParser.urlencoded({extended:false});
-
+app.use(cors());
 
 app.post('../sub',urlencodedParser, function(req,res ){
 
